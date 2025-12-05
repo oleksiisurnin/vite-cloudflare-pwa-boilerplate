8b92eff1137bfbfcc2e4eee26ea0475c30ee2487
Oleksii Surnin <bromkrololo@gmail.com>
2025-12-05 22:05:17 +0100

    Enhance project structure and update dependencies
    - Added `components.json` for component configuration.
    - Updated `.gitignore` to exclude `dev-dist`.
    - Enhanced `package.json` and `package-lock.json` with new dependencies including Tailwind CSS and Radix UI.
    - Refactored `App.tsx` to utilize a new Button component.
    - Configured TypeScript paths for easier imports.
    - Updated CSS to integrate Tailwind and custom theming.
    - Introduced utility functions for class name management.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +17,11 @@ export default defineConfig({
         enabled: true,
       },
     }),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
