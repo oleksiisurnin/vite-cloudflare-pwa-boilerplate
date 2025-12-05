12bf99995693d70c556f6087f8d416488cc76acb
Oleksii Surnin <bromkrololo@gmail.com>
2025-12-05 21:57:10 +0100

    Add Vite PWA support and update dependencies
    - Integrated `vite-plugin-pwa` for Progressive Web App capabilities.
    - Updated `vite.config.ts` to include PWA plugin configuration.
    - Modified `package.json` and `package-lock.json` to reflect new dependencies and versions.
    - Cleaned up unnecessary entries in `package-lock.json` for better maintainability.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+  plugins: [
+    react(),
+    cloudflare(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
